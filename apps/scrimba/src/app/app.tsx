a75e0290f6c1292741a8e76cbdb05163c6c7f8e9
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-09-13 14:18:28 +0200

    test: testing ci

--- apps/scrimba/src/app/app.tsx
+++ apps/scrimba/src/app/app.tsx
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
@@ -7,6 +6,7 @@ export function App() {
     <>
       <NxWelcome title="scrimba" />
       <div />
+      <footer>Footer</footer>
     </>
   );
 }
