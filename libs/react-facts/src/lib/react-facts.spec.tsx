26a9ad06d2cdde1ca6f9d084aab189d79d9b5514
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2022-09-13 18:43:52 +0200

    refactor: tsx syntax

--- libs/react-facts/src/lib/react-facts.spec.tsx
+++ libs/react-facts/src/lib/react-facts.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import ReactFacts from './react-facts';
+import { ReactFacts } from './react-facts';
 
 describe('ReactFacts', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ReactFacts />);
+    const { baseElement } = render(<ReactFacts baseUrl="/" />);
     expect(baseElement).toBeTruthy();
   });
 });
--- libs/react-facts/src/lib/react-facts.tsx
+++ libs/react-facts/src/lib/react-facts.tsx
@@ -2,14 +2,10 @@ import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { MainContent } from './components/MainContent';
 
-export function ReactFacts({ baseUrl }: { baseUrl: string }) {
-  return (
-    <div className="page">
-      <Header baseUrl={baseUrl} />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
-
-export default ReactFacts;
+export const ReactFacts = ({ baseUrl }: { baseUrl: string }) => (
+  <div className="page">
+    <Header baseUrl={baseUrl} />
+    <MainContent />
+    <Footer />
+  </div>
+);
